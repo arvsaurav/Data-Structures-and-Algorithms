e6388bcc0e6da472d5b5ab372eeaad4b3b626b53
Saurav Kumar <arvsaurav@gmail.com>
2021-07-08 01:16:47 +0530

    length of linked list.

---
+++ Linked List-I/LengthOfLL.java
@@ -0,0 +1,85 @@
+/*
+Problem Statement :
+For a given singly linked list of integers, find and return its length. Do it using an iterative method.
+*/
+
+public class Solution {
+
+	public static int length(LinkedListNode<Integer> head)
+    {
+		//Your code goes here
+        LinkedListNode<Integer> temp = head;
+        int count = 0;
+        while(temp != null)
+        {
+			count++;
+            temp = temp.next;
+        }
+        return count;
+	}
+}
+
+
+
+//Driver Code :
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+            
+            LinkedListNode<Integer> head = takeInput();    
+            System.out.println(Solution.length(head));
+            System.out.println();
+
+            t -= 1;
+        }
+    }
+}
